131e9f2ad4c3de87223795fd632fa794dcc6022a
NikolaStefanovskiMM <96490638+NikolaStefanovskiMM@users.noreply.github.com>
2022-03-21 15:21:53 +0100

    Add project files.

---
+++ Homework-02.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Homework_02._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsGOne = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] studentsGTwo = { "Zoki", "Mile", "Brus", "Petko", "Vele" };
+
+            Console.WriteLine("Enter student group: ");
+            string input = Console.ReadLine();
+            bool isParsed = int.TryParse(input, out int studentGroup);
+            if (!isParsed)
+            {
+                Console.WriteLine("Wrong input");
+                return;
+            }
+            switch (studentGroup)
+            {
+                case 1:
+                    Console.WriteLine($"The student from G1 are: \n {studentsGOne[0]}, {studentsGOne[1]}, {studentsGOne[2]}, {studentsGOne[3]}, {studentsGOne[4]}.");
+                    break;
+                case 2:
+                    Console.WriteLine($"The student from G1 are: \n {studentsGTwo[0]}, {studentsGTwo[1]}, {studentsGTwo[2]}, {studentsGTwo[3]}, {studentsGTwo[4]}.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+
+            }
+        }
+    }
+}
---
+++ Homework02/Program.cs
@@ -0,0 +1,54 @@
+﻿using Homework02.Entites;
+using System;
+
+namespace Homework02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = ReadNumbers();
+            int num2 = ReadNumbers();
+            int num3 = ReadNumbers();
+            int num4 = ReadNumbers();
+            int num5 = ReadNumbers();
+            int num6 = ReadNumbers();
+
+            int[] numberArr = new int[6];
+            numberArr[0] = num1;
+            numberArr[1] = num2;
+            numberArr[2] = num3;
+            numberArr[3] = num4;
+            numberArr[4] = num5;
+            numberArr[5] = num6;
+
+            int arrResult = SumOfEven(numberArr);
+            Console.WriteLine($"The result from all even numbers is: {arrResult}");
+
+
+        }
+        static int ReadNumbers()
+        {
+            Console.WriteLine("Enter a number");
+            string stringNumber = Console.ReadLine();
+            int number = int.Parse(stringNumber);
+            return number;
+        }
+
+        static int SumOfEven(int[] arrayOne)
+        {
+            //int[] arrayOne = { };
+            int evenNumresult = 0;
+            for (int counter = 0; counter < arrayOne.Length; counter++)
+            {
+                
+                if (arrayOne[counter] % 2 == 0)
+                {
+                    evenNumresult += arrayOne[counter];
+                }
+            }
+            return evenNumresult;
+        }
+
+    }
+}
